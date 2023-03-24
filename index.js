989851e04a3b7a10035c199ad1d396cb225e4a20
Abdela umer <adheremo65@gmail.com>
2023-03-23 22:49:35 -0400

    javascript function lab cont is done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(placeholder="roller-skate"){
+return `This Saturday, I want to ${placeholder}!`
+};
+mondayWork = function ( march = "go to the office"){
+    return `This Monday, I will ${march}.`}
+mondayWork();
+
+
+        function wrapAdjective(low ="||special||"){
+           return function (high ="*"){
+                return `You are *${high}*!`
+            } }   
+        
+        
+
+
+
+
+
+
